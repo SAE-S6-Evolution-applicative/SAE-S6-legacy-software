aa02251ad0f1efcf6f15331c257a6329ddba7343
François SP <francois-sp@gmx.fr>
2025-05-13 10:10:58 +0200

    test: set random port for RoomDaoImplIntegrationTest
    Updates @SpringBootTest annotation to use a random port
    for running integration tests, improving isolation and
    preventing port conflicts during test execution.

--- src/test/java/sae/semestre/six/appointment/room/RoomDaoImplIntegrationTest.java
+++ src/test/java/sae/semestre/six/appointment/room/RoomDaoImplIntegrationTest.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Transactional
 class RoomDaoImplIntegrationTest {
 
